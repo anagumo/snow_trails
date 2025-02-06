a98ffbf950291e05ac56a09ae115cae0e806e2d5
Ariana Nagumo <arinagumo@gmail.com>
2025-02-06 13:11:46 -0600

    feat(snow_trails): create project

---
+++ SnowTrails/SnowTrails/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  SnowTrails
+//
+//  Created by Ariana Rodríguez on 06/02/25.
+//
+
+import Foundation
+
+Main().main()
+
+class Main {
+    
+    // MARK: Main
+    func main() {
+        // TODO: Run the app here
+    }
+}
---
+++ SnowTrails/SnowTrailsTesting/SnowTrailsTesting.swift
@@ -0,0 +1,15 @@
+//
+//  SnowTrailsTesting.swift
+//  SnowTrailsTesting
+//
+//  Created by Ariana Rodríguez on 06/02/25.
+//
+
+import Testing
+
+class SnowTrailsTesting {
+    
+    @Test func firstTest() async throws {
+        // TODO: Write your first test here
+    }
+}
