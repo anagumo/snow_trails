e577faa2d4e2e8be4f4f0da1565ae7f5ae9a28cb
Ariana Nagumo <arinagumo@gmail.com>
2025-02-11 12:35:06 -0600

    test(snow_trails): add tests to users feat

--- SnowTrails/SnowTrailsTesting/SnowTrailsTesting.swift
+++ SnowTrails/SnowTrailsTesting/SnowTrailsTesting.swift
@@ -33,17 +33,6 @@ class SnowTrailsTesting {
             let userLoaded = userDataLoader.login(emailInput: "regularuser@keepcoding.es", passwordInput: "Regularuser1")
             #expect(userLoaded != nil, "It is expected that the user is in the data source")
         }
-        
-        @Test func logoutSuccess() async throws {
-            guard let userLoggedIn = userDataLoader.login(emailInput: "regularuser@keepcoding.es", passwordInput: "Regularuser1") else {
-                return
-            }
-            guard let userLoggedOut = userDataLoader.logout(userId: userLoggedIn.id) else {
-                return
-            }
-            
-            #expect(userLoggedOut.isLoggedIn == false, "It is expected that the user is logged out")
-        }
     }
     
     @Suite("Routes")
@@ -79,6 +68,59 @@ class SnowTrailsTesting {
             let routeDescription = routes.first?.getDescription()
             #expect(routeDescription == "Ruta del Refugio Aislado - 0.00 Km",
                     "It is expected that returns the formatted route's description")
+        }
+    }
+    
+    @Suite("Users")
+    class UsersTests {
+        var testData: Data?
+        let userDataLoader: UserDataLoader
+        
+        init() {
+            testData = UserTestData.givenTwoUsers() ?? "".data(using: .utf8)!
+            userDataLoader = UserDataLoader(from: testData)
+        }
+        
+        deinit {
+            testData = nil
+        }
+        
+        @Test func logoutSuccess() async throws {
+            guard let userLoggedIn = userDataLoader.login(emailInput: "regularuser@keepcoding.es", passwordInput: "Regularuser1") else {
+                return
+            }
+            guard let userLoggedOut = userDataLoader.logout(userId: userLoggedIn.id) else {
+                return
+            }
+            
+            #expect(userLoggedOut.isLoggedIn == false, "It is expected that the user is logged out")
+        }
+        
+        @Test func getUsers() async throws {
+            #expect(userDataLoader.getAll().count == 2, "It is expected to return 2 users")
+        }
+        
+        @Test func addUserSuccess() async throws {
+            let userAdded = userDataLoader.add(name: "Regularuserkeepcoding2", email: "regularuser2@keepcoding.es", password: "Regularuser2")
+            let userFound = userDataLoader.users.filter {
+                $0.username == userAdded?.username
+            }.first
+            
+            #expect(userFound?.id == "3", "It is expected the user id is the last one more one")
+        }
+        
+        @Test func deleteUserSuccess() async throws {
+            let userAdded = userDataLoader.add(name: "Regularuserkeepcoding2", email: "regularuser2@keepcoding.es", password: "Regularuser2")
+            let isDeletionSuccess = userDataLoader.delete(username: userAdded?.username ?? "")
+            
+            #expect(isDeletionSuccess, "It is expected to return true since there was a user in the loader")
+            #expect(userDataLoader.users.count == 2, "It is expected to return 2 users after deletion")
+        }
+        
+        @Test func deleteUserError() async throws {
+            let isDeletionSuccess = userDataLoader.delete(username: "AriKeepCoder")
+            
+            #expect(!isDeletionSuccess, "It is expected to return false since there is no a user in the loader")
         }
     }
 }
